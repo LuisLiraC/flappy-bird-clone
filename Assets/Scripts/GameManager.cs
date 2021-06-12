20e2cf79ca2c483bec0e80f57eb079de0ddf300c
LuisLiraC <luis_ssbm@hotmail.com>
2021-06-11 20:49:42 -0500

    Custom fonts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -104,7 +104,7 @@ public class GameManager : MonoBehaviour
         #if UNITY_EDITOR
                 UnityEditor.EditorApplication.isPlaying = false;
         #else
-                Application.Quit()
+                Application.Quit();
         #endif
     }
 
